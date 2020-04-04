99d7f1028b63b1b7059e0c412d1651b9a010acf4
Champonnois Victor <victor.champonnois@irstea.fr>
2020-04-04 18:59:54 +0200

    first toy game

---
+++ notes_algo.md
@@ -0,0 +1,23 @@
+---
+
+
+I followed the tutorials:
+https://www.youtube.com/watch?v=i6xMBig-pP4
+https://www.youtube.com/watch?v=2-DNswzCkqk 
+to get started with pygame, seting up the screen, 
+moving objects, placing boundaries.
+
+I wrote a toy program to follow the video.
+I then progressively modify this program to create my game.
+
+# Movement and boundaries
+Pygame  
+
+# walls
+https://www.youtube.com/watch?v=1aGuhUFwvXA
+https://www.youtube.com/watch?v=8IRyt7ft7zg
+
+
+# Using OOP 
+see video 
+https://www.youtube.com/watch?v=xfnRywBv5VM
--- toy_pygame.py
+++ toy_pygame.py
@@ -1,4 +1,49 @@
 import pygame
 
 pygame.init()
-win = pygame.window(500, 500)
+
+# window
+win_width = 500
+win_height = 500
+win = pygame.display.set_mode((win_width, win_height))
+pygame.display.set_caption("Toy")
+
+# set up 
+x = 50 # initial position of rectangle
+y = 50
+width = 40
+height = 60
+vel = 5 # velocity
+
+
+# board: coordinates are from the top left, moving left = x -1, moving up: y+1. 0;0 is the top lef
+
+# main loop
+run = True # run variable, wether game is active or not
+while run:
+    pygame.time.delay(100) # clock of the game, in millisecond
+    
+    # manual quit condition
+    for event in pygame.event.get():# list of all events that happened
+        if event.type == pygame.QUIT:
+            run = False
+
+    # coordinates of an object is the top left point of this object
+    # moving
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT] and x > vel: # vel and not zero because move by five units at a time
+        x -= vel
+    if keys[pygame.K_RIGHT] and x < win_width - vel - width:
+        x += vel
+    if keys[pygame.K_UP] and y > vel:
+        y -= vel
+    if keys[pygame.K_DOWN] and y < win_height - vel - width:
+        y += vel
+
+    # arguments: window, color(RBG), rectangle coord
+    win.fill((0,0,0)) # erase past rectangle
+    pygame.draw.rect(win, (255, 0, 0), (x, y, width, height))
+    # refresh display
+    pygame.display.update()
+
+pygame.quit()
