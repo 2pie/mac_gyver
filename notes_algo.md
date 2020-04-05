ccc79dc70cf9e0fb3fc2eb81ecb9aba4a73abf77
Champonnois Victor <victor.champonnois@irstea.fr>
2020-04-05 17:27:05 +0200

    Adding a wall

--- notes_algo.md
+++ notes_algo.md
@@ -2,22 +2,45 @@
 
 
 I followed the tutorials:
-https://www.youtube.com/watch?v=i6xMBig-pP4
-https://www.youtube.com/watch?v=2-DNswzCkqk 
+
 to get started with pygame, seting up the screen, 
 moving objects, placing boundaries.
 
 I wrote a toy program to follow the video.
-I then progressively modify this program to create my game.
+Since the tutorial was not very far from the type of game I had to design, I then progressively modify this program to create my game.
+
+I followed the following steps
 
 # Movement and boundaries
-Pygame  
+Create an empty screen, and a rectange that moves in this screen. Prevent rectangle from leaving the screen.
 
-# walls
+
+From the start, I used an Object oriented programming approach. I defined the object player.
+
+Then I added a class Wall stopping player in case of collision.
+
+Once I had the basic structure of the code (i.e. my player class and my wall class), I designed the shape of the labyrinth with blocks.
+
+
+
+# Reference
+
+## Starting with pygame
+https://www.youtube.com/watch?v=i6xMBig-pP4
+https://www.youtube.com/watch?v=2-DNswzCkqk
+
+## Walls
 https://www.youtube.com/watch?v=1aGuhUFwvXA
 https://www.youtube.com/watch?v=8IRyt7ft7zg
+http://programarcadegames.com/python_examples/show_file.php?file=move_with_walls_example.py
 
 
-# Using OOP 
-see video 
+## Using OOP 
 https://www.youtube.com/watch?v=xfnRywBv5VM
+
+
+# Extensions
+Simplifier le keydown/keyup? juste bouger si keydown ?
+Faire bouger l'image de MG selon direction de marche
+Image pour layrinthe
+sons etc.
--- toy_pygame.py
+++ toy_pygame.py
@@ -2,48 +2,140 @@ import pygame
 
 pygame.init()
 
-# window
+########################## Set up the game
+ 
 win_width = 500
 win_height = 500
 win = pygame.display.set_mode((win_width, win_height))
 pygame.display.set_caption("Toy")
+clock = 100                                     # clock of the game, in millisecond
 
-# set up 
-x = 50 # initial position of rectangle
-y = 50
-width = 40
-height = 60
-vel = 5 # velocity
+########################## Create objects
+
+class Player(pygame.sprite.Sprite):
+
+    def __init__(self, x, y, width, height):
+        
+        # Call the parent class (Sprite) constructor
+        pygame.sprite.Sprite.__init__(self)
+ 
+        # Create an image of the block, and fill it with a color.
+        # This could also be an image loaded from the disk.
+        self.image = pygame.Surface([width, height])
+        self.image.fill((255, 0, 0))
+ 
+        # Fetch the rectangle object that has the dimensions of the image
+        # image.
+        # Update the position of this object by setting the values
+        # of rect.x and rect.y
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.vel = 5
+
+        # Set move vector
+        self.change_x = 0
+        self.change_y = 0
+        self.walls = None
+ 
+    def move(self, change_x, change_y):
+        self.change_x += change_x
+        self.change_y += change_y
+        
+    def update(self):
+        " Update the player position."
+
+        # Move left/right
+        self.rect.x += self.change_x
+ 
+        # Do player hit a wall?
+        block_hit_list = pygame.sprite.spritecollide(self, self.walls, False)
+        for block in block_hit_list:
+            # If we are moving right, set our right side to the left side of
+            # the item we hit
+            if self.change_x > 0:
+                self.rect.right = block.rect.left
+            else:
+                self.rect.left = block.rect.right
+ 
+        # Move up/down
+        self.rect.y += self.change_y
+ 
+        # Do player hit a wall?
+        block_hit_list = pygame.sprite.spritecollide(self, self.walls, False)
+        for block in block_hit_list:
+ 
+            # Reset our position based on the top/bottom of the object.
+            if self.change_y > 0:
+                self.rect.bottom = block.rect.top
+            else:
+                self.rect.top = block.rect.bottom
+
+            
+class Block(pygame.sprite.Sprite):
+
+    def __init__(self, x, y, width, height):
+        """ Constructor. Pass in the color of the block,
+        and its size. """
+ 
+        # Call the parent class
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface([width, height])
+        self.image.fill((0, 255, 0))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+########################## Set up the game
+
+all_list = pygame.sprite.Group()
+block_list = pygame.sprite.Group()
+
+block = Block(60, 60, 10, 60)
+block_list.add(block)
+all_list.add(block)
+
+mg = Player(0, 0, 10, 10)
+mg.walls = block_list
+all_list.add(mg)
 
 
-# board: coordinates are from the top left, moving left = x -1, moving up: y+1. 0;0 is the top lef
+########################## Main loop 
 
-# main loop
-run = True # run variable, wether game is active or not
+run = True 
 while run:
-    pygame.time.delay(100) # clock of the game, in millisecond
+    pygame.time.delay(clock)
     
     # manual quit condition
     for event in pygame.event.get():# list of all events that happened
         if event.type == pygame.QUIT:
             run = False
 
-    # coordinates of an object is the top left point of this object
-    # moving
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT] and x > vel: # vel and not zero because move by five units at a time
-        x -= vel
-    if keys[pygame.K_RIGHT] and x < win_width - vel - width:
-        x += vel
-    if keys[pygame.K_UP] and y > vel:
-        y -= vel
-    if keys[pygame.K_DOWN] and y < win_height - vel - width:
-        y += vel
-
-    # arguments: window, color(RBG), rectangle coord
-    win.fill((0,0,0)) # erase past rectangle
-    pygame.draw.rect(win, (255, 0, 0), (x, y, width, height))
-    # refresh display
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                mg.move(-mg.vel, 0)
+            elif event.key == pygame.K_RIGHT:
+                mg.move(mg.vel, 0)
+            elif event.key == pygame.K_UP:
+                mg.move(0, -mg.vel)
+            elif event.key == pygame.K_DOWN:
+                mg.move(0, mg.vel)
+ 
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT:
+                mg.move(mg.vel, 0)
+            elif event.key == pygame.K_RIGHT:
+                mg.move(-mg.vel, 0)
+            elif event.key == pygame.K_UP:
+                mg.move(0, mg.vel)
+            elif event.key == pygame.K_DOWN:
+                mg.move(0, -mg.vel)
+    all_list.update()
+    
+    # Draw
+    win.fill((0,0,0))
+    all_list.draw(win)
     pygame.display.update()
+
 
 pygame.quit()
