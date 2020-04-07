205795fa55b7e7e586e60cbaaa97e6de84a1120c
Champonnois Victor <victor.champonnois@irstea.fr>
2020-04-07 12:23:10 +0200

    full working version of the game

--- toy_pygame.py
+++ toy_pygame.py
@@ -1,5 +1,6 @@
 import pygame
 import csv
+import random
 
 pygame.init()
 
@@ -7,6 +8,7 @@ pygame.init()
  
 win_width = 600
 win_height = 600
+n_item = 3
 # n_sprites = 15
 # sprite_width = win_height/n_sprites
 # sprite_width = win_width/n_sprites
@@ -15,7 +17,7 @@ win = pygame.display.set_mode((win_width, win_height))
 pygame.display.set_caption("Help MacGyver Escape!")
 clock = 100 # clock of the game, in millisecond
 
-########################## Create objects
+########################## Define classes
 
 class Player(pygame.sprite.Sprite):
 
@@ -36,20 +38,21 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
-        self.vel = 8
+        self.vel = 10
 
         # Set move vector
         self.change_x = 0
         self.change_y = 0
         self.walls = None
         self.guard = None
+        self.item = None
+        self.score = 0
  
     def move(self, change_x, change_y):
         self.change_x += change_x
         self.change_y += change_y
         
     def update(self):
-        " Update the player position."
 
         # Move left/right
         self.rect.x += self.change_x
@@ -57,8 +60,7 @@ class Player(pygame.sprite.Sprite):
         # Do player hit a wall?
         block_hit_list = pygame.sprite.spritecollide(self, self.walls, False)
         for block in block_hit_list:
-            # If we are moving right, set our right side to the left side of
-            # the item we hit
+            # If we are moving right, set our right side to the left side of the wall we hit
             if self.change_x > 0:
                 self.rect.right = block.rect.left
             else:
@@ -70,89 +72,116 @@ class Player(pygame.sprite.Sprite):
         # Do player hit a wall?
         block_hit_list = pygame.sprite.spritecollide(self, self.walls, False)
         for block in block_hit_list:
- 
-            # Reset our position based on the top/bottom of the object.
             if self.change_y > 0:
                 self.rect.bottom = block.rect.top
             else:
                 self.rect.top = block.rect.bottom
         
-        # Do player hit Guardien?
+        # Guardian interaction
         guard_hit = pygame.sprite.spritecollide(self, self.guard, False)
         if guard_hit:
-            global victory
-            victory = True
+            if self.score == n_item:
+                global victory
+                victory = True
+            
+            if self.score < n_item:
+                global defeat
+                defeat = True
 
             if self.change_x > 0:
                 self.rect.right = guard.rect.left
             else:
                 self.rect.left = guard.rect.right
-
-
-
+        
+        # Item interaction
+        item_hit = pygame.sprite.spritecollide(self, self.item, True)
+        if item_hit:
+            self.score += 1
 
             
-class Wall(pygame.sprite.Sprite):
+class Block(pygame.sprite.Sprite):
 
-    def __init__(self, x, y, width, height):
+    def __init__(self, x, y, width, height, color = (0, 255, 0)):
 
         # Call the parent class
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.Surface([width, height])
-        self.image.fill((0, 255, 0))
+        self.image.fill(color)
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
 
-class Guardian(pygame.sprite.Sprite):
 
-    def __init__(self, x, y, width, height):
- 
-        pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface([width, height])
-        self.image.fill((0, 0, 255))
-        self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y
 
 ########################## Create instances
 
 all_list = pygame.sprite.Group()
-block_list = pygame.sprite.Group()
+wall_list = pygame.sprite.Group()
 guard_list = pygame.sprite.Group()
+item_list = pygame.sprite.Group()
 
 
 # Walls
-with open('blocks.csv') as csv_file:
+with open('blocks.csv') as csv_file: # REMOVE
     csv_reader = csv.reader(csv_file, delimiter = ',')
     for row in csv_reader:
-        block = Wall(int(row[0]), int(row[1]), int(row[2]), int(row[3]))
-        block_list.add(block)
+        block = Block(int(row[0]), int(row[1]), int(row[2]), int(row[3]))
+        wall_list.add(block)
         all_list.add(block)
 
 # Border of the labyrinth
-block_l = Wall(0, 0, 0, 600)
-block_u = Wall(0, 0, 600, 0)
-block_d = Wall(0, 600, 600, 0)
-block_r = Wall(600, 0, 600, 0)
+block_l = Block(0, 0, 0, 600)
+block_u = Block(0, 0, 600, 0)
+block_d = Block(0, 600, 600, 0)
+block_r = Block(600, 0, 0, 600)
 all_list.add(block_l)
 all_list.add(block_u)
 all_list.add(block_d)
 all_list.add(block_r)
-block_list.add(block_l)
-block_list.add(block_u)
-block_list.add(block_d)
-block_list.add(block_r)
+wall_list.add(block_l)
+wall_list.add(block_u)
+wall_list.add(block_d)
+wall_list.add(block_r)
 
 # Guardian
-guard = Guardian(280, 0, 40, 40)
+guard = Block(280, 0, 40, 40, (0, 0, 255))
 all_list.add(guard)
 guard_list.add(guard) # need to have a list for spritecollide
 
+# Items METTRE EN CLASSE ?
+n = 1
+xy_wall = []
+csv_reader = csv.reader(open('blocks.csv'), delimiter = ',')
+for row in csv_reader:
+    xy_wall.append((int(row[0]), int(row[1])))
+
+while n <= n_item:
+    # generate item position
+    x_item = random.randint(0, 14)
+    x_item = x_item * 40
+    y_item = random.randint(0, 14)
+    y_item = y_item * 40
+    xy_item = (x_item, y_item)
+
+    # check if not in a wall
+    match = False
+    for wall in xy_wall:
+        if xy_item == wall:
+            match = True
+    
+    # if not, create the item instance
+    if match == False:
+        # everythin good, add item and next
+        item = Block(xy_item[0], xy_item[1], 40, 40, (0, 255, 255))
+        all_list.add(item)
+        item_list.add(item)
+        n += 1
+
 # Player
 mg = Player(280, 560, 40, 40)
-mg.walls = block_list
+mg.walls = wall_list
 mg.guard = guard_list
+mg.item = item_list
 all_list.add(mg)
 
 
@@ -160,15 +189,15 @@ all_list.add(mg)
 
 run = True 
 victory = False
+defeat = False
 while run:
     pygame.time.delay(clock)
     
-    # manual quit condition
+    # Get events
     for event in pygame.event.get():# list of all events that happened
         if event.type == pygame.QUIT:
             run = False
         
-        # moving
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
                 mg.move(-mg.vel, 0)
@@ -190,12 +219,13 @@ while run:
                 mg.move(0, -mg.vel)
 
 
-    
-    all_list.update()
-
     # Draw
+    all_list.update()
     win.fill((0,0,0))
     all_list.draw(win)
+    font = pygame.font.Font(pygame.font.get_default_font(), 24)
+    text = font.render("Score = " + str(mg.score), True, (255, 255, 255))
+    win.blit(text, dest = (0,0))
     pygame.display.update()
 
     if victory:
@@ -206,6 +236,12 @@ while run:
         pygame.time.delay(1000)
         run = False
     
-
+    if defeat:
+        font = pygame.font.Font(pygame.font.get_default_font(), 36)
+        text = font.render("You lose !", True, (255, 255, 255))
+        win.blit(text, dest=(200,200))
+        pygame.display.update()
+        pygame.time.delay(1000)
+        run = False
 
 pygame.quit()
